eb2093cb6f891cf073ab0b4af08b4189f6a12b8a
Caesar Wang <wxt@rock-chips.com>
2024-11-20 19:27:16 +0800

    chromium: update test_simplebrowser.sh
    Change-Id: I1c561fc4f3cecc5dd251b9c2288beb9946758549
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- chromium/test_simplebrowser.sh
+++ chromium/test_simplebrowser.sh
@@ -1,15 +1,25 @@
 #!/bin/bash -e
 
+
+# Export mpp_syslog_perror
 export mpp_syslog_perror=1
 
-KERNEL_VERSION=$(cat /proc/version)
-if [[ $KERNEL_VERSION =~ "4.4" ]]; then
+# Get kernel version
+KERNEL_VERSION=$(uname -r)
+
+# Set rk_vcodec debug parameter based on kernel version
+if echo "$KERNEL_VERSION" | grep -q '^4\.4'; then
     echo 0x100 > /sys/module/rk_vcodec/parameters/debug
 else
     echo 0x100 > /sys/module/rk_vcodec/parameters/mpp_dev_debug
 fi
 
-echo performance | tee $(find /sys/ -name *governor)
+## Set CPU governor to performance
+# Find all CPU governor files
+GOVERNOR_FILES=$(find /sys/ -name *governor)
+
+# Set governor to performance
+echo performance | tee $GOVERNOR_FILES > /dev/null 2>&1 || true
 
 if [ -e "/usr/lib/qt/examples/webenginewidgets/simplebrowser" ] ;
 then
